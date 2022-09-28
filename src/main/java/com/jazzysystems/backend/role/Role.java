4222c672ec1ab94c0e14f2ca1a7638c9f75e979f
Juan Sanchez <jusanchezme@unal.edu.co>
2022-09-27 21:54:26 -0500

    Add index to Role

--- src/main/java/com/jazzysystems/backend/role/Role.java
+++ src/main/java/com/jazzysystems/backend/role/Role.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -15,7 +16,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
-@Table(name = "Role")
+@Table(name = "Role", indexes = @Index(name = "uniqueRole", columnList = "roleName", unique = true))
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
