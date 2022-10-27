aa2209a051d015c9a5faf314c2d3aa97e02480c9
Juan Sanchez <jusanchezme@unal.edu.co>
2022-10-27 11:49:09 -0500

    Fix Mapper

--- src/main/java/com/jazzysystems/backend/typeRequest/TypeRequestMapper.java
+++ src/main/java/com/jazzysystems/backend/typeRequest/TypeRequestMapper.java
@@ -1,9 +1,11 @@
 package com.jazzysystems.backend.typeRequest;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
 import com.jazzysystems.backend.typeRequest.dto.TypeRequestDTO;
 
+@Component
 public class TypeRequestMapper {
     public TypeRequestDTO convertToDTO(TypeRequest typeRequest) {
         ModelMapper modelMapper = new ModelMapper();
